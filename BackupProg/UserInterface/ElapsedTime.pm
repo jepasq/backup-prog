2409418c8002b650d896f49e66c06829134fdc6b
Jepasq <jepasq111@gmail.com>
2022-08-30 14:55:03 +0200

    Start to print time in logger

--- BackupProg/UserInterface/ElapsedTime.pm
+++ BackupProg/UserInterface/ElapsedTime.pm
@@ -2,7 +2,10 @@ use strict;
 
 package BackupProg::UserInterface::ElapsedTime;
 
+use BackupProg::Common::Logger;
 use BackupProg::UserInterface::Widget;
+
+use DateTime;
 
 our @ISA = qw(BackupProg::UserInterface::Widget);
     
@@ -14,6 +17,16 @@ sub new() {
     my $self = $class->SUPER::new('zze', @args);
     $self->{label}= 'ertetr';
     $self->draw_label();
+
+    my $log = BackupProg::Common::Logger->instance();
+    my $sttime = DateTime->now();
+    sleep(61);
+    my $entime = DateTime->now;
+    my $elapse = $entime - $sttime;
+    $log->LOGI("Elapsed time : ".$elapse->in_units('seconds')."s");
+    $log->LOGI("Elapsed time : ".$elapse->in_units('minutes')."m");
+    $log->LOGI("Elapsed time : ".$elapse->in_units('hours')."h");
+
     # Already blessed by parent
     return $self;
 }
