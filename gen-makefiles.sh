10cfbf0a017668ad279e887846168a63eade0ef0
Jepasq <jepasq111@gmail.com>
2022-09-08 21:21:21 +0200

    Add a m4 existence check in gen-makefiles.sh

--- gen-makefiles.sh
+++ gen-makefiles.sh
@@ -11,6 +11,15 @@ SUBDIRS=". \
     BackupProg/Parser \
     BackupProg/UserInterface"
 
+wm4=`m4 --help`
+wm4_es=$?
+if [ $wm4_es -eq 127 ]
+then
+    echo -n "  Can't find the m4 binary. Please read README.md to know"
+    echo " how to install needed packages."
+    exit 1
+fi    
+
 for d in $SUBDIRS; do (
 	echo "Generating $OUT for directory $d/ ..."
 	cd $d && $M4 $IN > $OUT 
