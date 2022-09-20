786a6b99e16bdc0c0b688daa2226aef92ce97b5a
Jepasq <jepasq111@gmail.com>
2022-09-20 03:39:53 +0200

    First ActionList drawActions implementation

--- BackupProg/UserInterface/ActionList.pm
+++ BackupProg/UserInterface/ActionList.pm
@@ -18,12 +18,12 @@ sub new() {
     # possibly call Parent->new(@args) first
     my $self = $class->SUPER::new('Actions:', @args);
 
-    my $log = BackupProg::Common::Logger->instance();
+    $self->{log} = BackupProg::Common::Logger->instance();
     #addstr(10, 10, $self->get_elapsed_str());
     $self->{win}->addstr(1, 1, "Just testin'");
     
     #$self->draw_label();
-    
+    $self->draw_actions();
     # Already blessed by parent
     return $self;
 }
@@ -39,6 +39,17 @@ sub append(){
      
      push @actions, $val;
      $self->{log}->LOGI("ActionList: (@actions)in len is now ". $self->len());
+}
+
+sub draw_actions(){
+    my $self = shift;
+    my $action_number = $self->len();
+    $self->{log}->LOGI("Printing ActionList with $action_number items...");
+
+    for (my $i = 0; $i <= $action_number; $i++) {
+	my $my= 3 + $i;
+	$self->{win}->addstr($my, 1, $actions[$i]);
+    }
 }
 
 1;
