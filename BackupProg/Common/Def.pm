1a348d9e1dc1b988010221908e4256f212fec30b
Jepasq <jepasq111@gmail.com>
2022-11-24 16:39:41 +0100

    Switch to revision 19
    - Add some pre-call tests to Widget::refresh to fix unit tests
    - Improve label and box widget handling in unit tests
    - Bump version to 0.0.2 (5 ROADMAP sections closed)
    - Fix the UI refresh issue
    - Partly fixed bless-related Widget issue
    - More get_window tests
    - Better self call in Widget ctor implementation
    - Creating a new parent label works to update label

--- BackupProg/Common/Def.pm
+++ BackupProg/Common/Def.pm
@@ -37,7 +37,7 @@ sub new{
     my $self = bless { }, $class;
 
     $self->{ PROGNAME } = "backup_prog";
-    $self->{ VERSION }  = "0.0.1-18";
+    $self->{ VERSION }  = "0.0.1-19";
     $self->{ DATE }     = get_today();
     $self->{ USER }     = getlogin();
 
