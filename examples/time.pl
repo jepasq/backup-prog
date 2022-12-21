95067f000575a11be0d15861682a497f5a8f4832
Jepasq <jepasq111@gmail.com>
2022-12-21 03:31:41 +0100

    time.pl uses a endless while loop

--- examples/time.pl
+++ examples/time.pl
@@ -7,14 +7,15 @@ use POSIX qw(strftime);
 
 # Based on code from https://www.perlmonks.org/?node_id=783615
 my $count=1;
-my @time=(4,12,16,8);
 
 print "Start at $count\n";
 my $lastseconds  = -1;
-foreach my $t (@time) {
-    my $date = strftime "%S", localtime $t;
+while (1) {
+    my $date = strftime "%S", localtime;
+    print ".";
     if ($date != $lastseconds) {
 	$lastseconds = $date;
-	print "New second ($t)\n";
+	print "New second ($date => $count++)\n";
     }
+    sleep(0.1);
 }
