5358ce0ddb64b85961cfb6325b12a6c56bce5b9b
Jepasq <jepasq111@gmail.com>
2022-12-23 04:17:33 +0100

    Improve time.pl example to autoflush output

--- examples/time.pl
+++ examples/time.pl
@@ -4,18 +4,25 @@ use warnings;
 
 use Time::HiRes qw(time);
 use POSIX qw(strftime);
+use IO::Handle;
 
 # Based on code from https://www.perlmonks.org/?node_id=783615
-my $count=1;
+my $count=0;
 
-print "Start at $count\n";
+# Needed to update terminal wihout newline
+STDOUT->autoflush(1);
+
+print "Newline every 10s\n";
 my $lastseconds  = -1;
 while (1) {
     my $date = strftime "%S", localtime;
-    print ".";
     if ($date != $lastseconds) {
 	$lastseconds = $date;
-	print "New second ($date => $count++)\n";
+	print ".";
+	$count++;
+	if (($count % 10) == 0) {
+	    print "\n";
+	}
     }
     sleep(0.1);
 }
