526081fd2e6b026ef5a3ee5de4f542eb9f02de61
Jepasq <jepasq111@gmail.com>
2023-05-30 04:50:56 +0200

    Trying to build a newwin in MainMenu

--- BackupProg/UserInterface/MainMenu.pm
+++ BackupProg/UserInterface/MainMenu.pm
@@ -1,5 +1,7 @@
 use strict;
 
+use Curses;
+
 package BackupProg::UserInterface::MainMenu;
 
 sub new(){
@@ -16,5 +18,15 @@ sub show(){
     # y is the label_y value
     my $self = shift;
     print "Showing menu\n";
+
+    my $x = 5;
+    my $y = 5;
+    my $h = 10;
+    my $w = 15;
+    
+    # Create menu and add border
+    my $win = newwin($h, $w, $y, $x);
+    $win->box($win, 0, 0);
+
 }
 1;
