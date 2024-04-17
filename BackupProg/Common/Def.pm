321054033613f73dfb2ec587c66cbb4ed7cbc7f5
Jepasq <jepasq111@gmail.com>
2024-04-17 12:04:40 +0200

    Switch to revision 26
    - Remove root GEN_INFO rile and close related ROADMAP section
    - Improve README with some usefull links
    - Move GEN_INFO_RULES m4 macro to manual/ directory
    - Compress texinfo.tex file with gzip to acoid counting its SLOC
    - Improve README on texinfo manual preview and first manual chapter
    - Fix bad texinfo.tex file and move to manual directory
    - Add texinfo.tex file from GNU Texinfo distribution
    - Upgrade checkout action from v3 to v4 trynig to fix a node 0.16 warning

--- BackupProg/Common/Def.pm
+++ BackupProg/Common/Def.pm
@@ -37,7 +37,7 @@ sub new{
     my $self = bless { }, $class;
 
     $self->{ PROGNAME } = "backup_prog";
-    $self->{ VERSION }  = "0.0.1-25";
+    $self->{ VERSION }  = "0.0.1-26";
     $self->{ DATE }     = get_today();
     $self->{ USER }     = getlogin();
 
